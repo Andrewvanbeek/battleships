607021666685d5cc7881d34c2d20bc841233b8a1
Andrew Van Beek <andrewvanbeek90gmail.com>
2016-12-07 14:38:00 -0600

    adds css

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+end
