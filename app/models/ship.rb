f62ce1b7d3fb6687fc773d731a787f2063efeae9
TrRo26 <travis_roy@yahoo.com>
2016-12-07 16:57:44 -0600

    Add FUCKING comma

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -5,5 +5,5 @@ class Ship < ActiveRecord::Base
 	validates :size, presence: true, :numericality => { greater_than: 1, less_than_or_equal_to: 5}
 	validates :x_coord, presence: true, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 7}
 	validates :y_coord, presence: true, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 7}
-	validates :hit_count, presence: true :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+	validates :hit_count, presence: true, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
 end
