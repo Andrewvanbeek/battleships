f69ab6cbccfa436c3c6c41a70df03279f1bfcd70
Danny Kalemba <email": null>
2016-12-07 15:16:54 -0600

    Add ability for user to log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "games#index"
   resources :users, :except => [:index]
+  resources :games
+  resources :sessions, only: [:new, :create]
+   delete "/logout" => "sessions#destroy"
 end
