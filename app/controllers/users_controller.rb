e8e5c32706d5791fefe2e2756d5341411cf6ad58
Danny Kalemba <email": null>
2016-12-07 13:34:51 -0600

    Add bcrypt

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, only: [:edit, :update, :show, :destroy]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+end
