4ee61c05e15c61d0770312a73245a6b39ab8d470
TrRo26 <travis_roy@yahoo.com>
2016-12-07 19:36:14 -0600

    Add stats view and stats methods to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,34 @@ class User < ActiveRecord::Base
   has_many :games
 
   has_secure_password
+
+	def total_win_count
+	 	user_id = self.id
+	 	wins = 0
+	 	self.games.each do |game|
+			if user_id == game.winner_id
+				wins += 1
+			end
+		end
+		wins
+	end
+
+		# self.games.where(winner_id: self.id)
+
+	def total_loss_count
+		self.games.length - self.games.user_total_win_count
+	end 
+
+	def total_shots_fired
+	 	self.shots.length
+	end
+
+	def total_hits
+		self.shots.where(hit: true).length
+	end
+
+	def total_misses
+		self.total_shots_fired - self.total_hits
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "games#index"
   resources :users, :except => [:index]
+  resources :games, :except => [:destroy, :edit, :update]
 end
