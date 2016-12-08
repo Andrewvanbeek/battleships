8e67638ec5e865771e130330582f50ca797c7245
Andrew Van Beek <andrewvanbeek90gmail.com>
2016-12-07 19:01:21 -0600

    views, ajax, shots, and tests for shots

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root "sessions#new"
-   resources :games
-  resources :sessions
-  resources :users
-  resources :shots, only: [:create, :index]
-=======
   root to: "users#new"
   resources :users, :except => [:index]
   resources :games, :except => [:edit, :update, :destroy]
   resources :sessions, only: [:new, :create]
+  resources :shots, only: [:create, :index]
   delete "/logout" => "sessions#destroy"
-
->>>>>>> origin/game
 end
