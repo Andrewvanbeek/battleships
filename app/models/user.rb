b96e72be538008210bc2742e9929ef354dc69ee9
Sean Kung <email": null>
2016-12-08 11:14:45 -0600

    Add playerX_ship methods and ready_to_start method, pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :shots
   has_many :games
-  has_many :ships, through: :games
+  has_many :ships
 
   has_secure_password
   validates :username, :email, presence: true
