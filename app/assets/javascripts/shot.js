bc160ad8541b56f11a644dd7850b45823a2bdb9b
Sean Kung <email": null>
2016-12-08 21:52:52 -0600

    Add games/game_id/shots route, create shots, adjust who_fired_last

--- app/assets/javascripts/shot.js
+++ app/assets/javascripts/shot.js
@@ -1,9 +1,9 @@
 $(document).on('ready page:load', function () {
    var updateShots = function () {
+
     $.ajax({
       url: window.location.pathname,
-      method: "GET",
-      data: {param1: 'value1'},
+      method: "GET"
     })
     .fail(function() {
       setTimeout(updateShots, 100);
@@ -12,38 +12,29 @@ $(document).on('ready page:load', function () {
       if(data.ready === true && ($("#playername").attr("name") === data.player)){
         console.log("IT WAS TRUE")
 
-         $("#opponent").on("click", ".space", function(e){
-      e.preventDefault();
+        $("#opponent").on("click", ".space", function(e){
+          e.preventDefault();
 
-      var $shot = $(this);
-      var x_coord = $shot.attr("value");
-      var y_coord = $shot.closest("tr").attr("value");
-      var data = {x_coord: x_coord, y_coord: y_coord};
-      console.log(x_coord)
-      console.log(y_coord)
-
-      $.ajax({
-        url: '/shots',
-        method: "POST",
-        data: data
-      }) .fail(function() {
-      }) .done(function(data) {
-        $shot.addClass("miss");
-      });
-  })
-
-
-
-
-
+          var $shot = $(this);
+          var x_coord = $shot.attr("value");
+          var y_coord = $shot.closest("tr").attr("value");
+          var data = {x_coord: x_coord, y_coord: y_coord};
+          debugger
+          $.ajax({
+            url: '/shots',
+            method: "POST",
+            data: data
+          }) .fail(function() {
+          }) .done(function(data) {
+            $shot.addClass("miss");
+          });
+        })
       }
       console.log(data.ready)
       setTimeout(updateShots, 1000);
     });
-
   };
   setTimeout(updateShots, 1000);
-
 });
 
 
--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,7 +1,9 @@
 class ShotsController < ApplicationController
   def create
     if request.xhr?
-      @shot = Shot.create(x_coord: params[:x_coord], x_coord: params[:y_coord])
+      @user = User.find_by(id: session[:user_id])
+      @game = Game.find_by(id: params[:game_id])
+      @shot = Shot.create(x_coord: params[:x_coord], x_coord: params[:y_coord], game_id: @game.id, user_id: @user_id)
      render json: {
       }, status: 201
     end
